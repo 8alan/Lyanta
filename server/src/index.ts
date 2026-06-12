605a6385f30e8c03b5162d1196ff2d5c3b45c5b3
Alan Ramirez <slovarez@gmail.com>
2026-06-11 23:30:21 -0400

    updated CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ app.use(helmet())
 
 // CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://lantana-rho.vercel.app', 'https://myriapods.com'],
+  origin: ['http://localhost:5173/', 'http://localhost:5174/', 'https://lantana-rho.vercel.app/', 'https://myriapods.com/'],
   credentials: true
 }))
 
